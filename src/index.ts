5c8a75fd384bc7a23fcdcc76dea4eaeeb41bd209
sahilrush <sahilrevankar20co21@gmail.com>
2024-12-08 22:03:48 +0530

    adding gitignore

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,11 @@
 import { ReceiveMessageCommand, SQSClient } from "@aws-sdk/client-sqs";
+import type {S3Event} from "aws-lambda"
+
 import * as dotenv from "dotenv"
 dotenv.config()
 
 const client = new SQSClient({
+    region:"us-east-1",
     credentials:{
         accessKeyId:process.env.AWS_ACCESS_KEY_ID as string,
         secretAccessKey:process.env.AWS_SECRET_ACCESS_KEY as string,
@@ -13,9 +16,24 @@ const client = new SQSClient({
 async function init() {
     const command = new ReceiveMessageCommand({
         QueueUrl:process.env.AWS_QUEUE_URL,
-        MaxNumberOfMessages:1
+        MaxNumberOfMessages:1,
+        WaitTimeSeconds:20
     });
+
+
+    while(true) {
+        const {Messages} = await client.send(command)
+        if(!Messages){
+            console.log(`No message in Queue `,);
+            continue;
+        }
+
+        for (const message of Messages){
+            const {Body, MessageId} = message;
+            console.log(`Message Received`, {MessageId,Body})
+        }
+    }
 
 }
 
-
+init();
