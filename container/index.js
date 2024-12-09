6262ff3477ddb34cbc80f52e9d05d83a86812e29
sahilrush <sahilrevankar20co21@gmail.com>
2024-12-09 20:31:41 +0530

    removing node_moduels

--- container/index.js
+++ container/index.js
@@ -1,8 +1,78 @@
+const { S3Client } = require('@aws-sdk/client-s3')
+const fs = require("node:fs/promises");
+const path = require("node:path")
+
+const ffmpeg = require("fluent-ffmpeg");
+
+const RESOLUTIONS = [
+    { name: "360p", width: 480, height: 360 },
+    { name: "480", width: 858, height: 480 },
+    { name: "720p", width: 1280, height: 720 },
+
+]
+
+const fs = require("fs")
+
+
+const s3Client = new S3Client({
+    region: "us-east-1",
+    credentials: {
+        accessKeyId: process.env.AWS_ACCESS_KEY_ID,
+        secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY,
+    }
+})
+
 //Download the original video
 
 
 
-//start the transcoder
 
 
-//upload the video
+
+async function init() {
+    
+    const command = new GetObjectCommand({
+        Bucket: process.env.BUCKET_NAME,
+        Key: process.env.KEY,
+    })
+    
+    const result = await s3Client.send(command)
+    
+    const originalFilePath = `videos/original-video.mp4`;
+    
+    
+    await fs.writeFile(originalFilePath, result.body)
+    
+    const originalVideoPath = path.resolve(originalFilePath)
+    
+    //start the transcoder
+    const promises = RESOLUTIONS.map(resolution => {
+        const output = `transcoded/video-${resolution.name}.mp4`
+        
+        return new Promise((resolve) => {
+            ffmpeg(originalVideoPath)
+            .output(output)
+            .withVideoCodec("libx264")
+            .withAudioCodec("aac")
+            .withSize(`${resolution.width}x${resolution.height}`)
+            .on("end", async () => {
+                const putCommand = new PutObjectCommand({
+                    Bucket:"",
+                });
+                await s3Client.send(putCommand);
+                console.log(`uploaded ${output}`);
+                resolve();
+            })
+            .format("mp4")
+            .run();
+        })
+        
+    })
+    
+    await Promise.all(promises);
+    //upload the video
+
+
+
+
+}
